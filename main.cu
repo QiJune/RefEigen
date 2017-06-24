ec340d83b83d0cc1525293c3d2e88028ea44d6ce
qijun <qijun1994@hotmail.com>
2017-06-24 15:14:24 +0800

    add mul test

--- main.cu
+++ main.cu
@@ -4,6 +4,7 @@
 #endif
 
 #include <iostream>
+#include <cmath>
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
@@ -24,9 +25,7 @@ void print(T* input, int size) {
   std::cout << std::endl;
 }
 
-int main() {
-
-  int size = 10;
+void test_add(int size) {
   float* t_a = (float*)malloc(size * sizeof(float));
   float* t_b = (float*)malloc(size * sizeof(float));
   float* t_c = (float*)malloc(size * sizeof(float));
@@ -34,7 +33,7 @@ int main() {
     t_a[i] = i;
     t_b[i] = i;
   }
-  
+
 #ifndef ONLY_CPU
   float* d_a;
   float* d_b;
@@ -45,18 +44,18 @@ int main() {
 
   cudaMemcpy(d_a, t_a, size * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, t_b, size * sizeof(float), cudaMemcpyHostToDevice);
-  
+
   Vec<float> a(d_a, size);
   Vec<float> b(d_b, size);
   Vec<float> c(d_c, size);
- 
+
   Eigen::CudaStreamDevice sd;
   Eigen::GpuDevice dd(&sd);
   c.device(dd) = a + b;
 
   cudaMemcpy(t_c, d_c, size * sizeof(float), cudaMemcpyDeviceToHost);
   print<float>(t_c, size);
-  
+
   cudaFree(d_a);
   cudaFree(d_b);
   cudaFree(d_c);
@@ -75,6 +74,71 @@ int main() {
   free(t_a);
   free(t_b);
   free(t_c);
+}
 
+void test_mul(int size) {
+
+  int width = sqrt(size);
+  int height = width;
+  float* t_a = (float*)malloc(size * sizeof(float));
+  float* t_b = (float*)malloc(size * sizeof(float));
+  float* t_c = (float*)malloc(size * sizeof(float));
+  for (int i = 0; i < size; i++) {
+    t_a[i] = i;
+    t_b[i] = i;
+  }
+
+  Eigen::array<Eigen::IndexPair<Eigen::DenseIndex>, 1> dim_pair;
+  dim_pair[0].first = 1;
+  dim_pair[0].second = 0;
+
+#ifndef ONLY_CPU
+  float* d_a;
+  float* d_b;
+  float* d_c;
+  cudaMalloc((void**)&d_a, size * sizeof(float));
+  cudaMalloc((void**)&d_b, size * sizeof(float));
+  cudaMalloc((void**)&d_c, size * sizeof(float));
+
+  cudaMemcpy(d_a, t_a, size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, t_b, size * sizeof(float), cudaMemcpyHostToDevice);
+
+  Matrix<float> a(d_a, height, width);
+  Matrix<float> b(d_b, height, width);
+  Matrix<float> c(d_c, height, width);
+
+  Eigen::CudaStreamDevice sd;
+  Eigen::GpuDevice dd(&sd);
+  c.device(dd) = a.contract(b, dim_pair);
+
+  cudaMemcpy(t_c, d_c, size * sizeof(float), cudaMemcpyDeviceToHost);
+  print<float>(t_c, size);
+
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+
+#else
+
+  Matrix<float> a(t_a, height, width);
+  Matrix<float> b(t_b, height, width);
+  Matrix<float> c(t_c, height, width);
+
+
+
+
+  Eigen::DefaultDevice dd;
+  c.device(dd) = a.contract(b, dim_pair);
+  print<float>(t_c, size);
+
+#endif
+  free(t_a);
+  free(t_b);
+  free(t_c);
+}
+
+int main() {
+  test_add(10);
+  test_mul(4);
   return 0;
 }
